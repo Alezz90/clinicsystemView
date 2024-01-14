8b31c3da62e8fdc104ad62fa846aada0c022eeb2
Alezz <alezzalmomn@gmail.com>
2024-01-14 14:00:31 +0400

    Repo

---
+++ SystemClinc.BLL/Repository/AdminRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemClinc.BLL.Interface;
+
+namespace SystemClinc.BLL.Repository
+{
+    internal class AdminRepository : GenaricRepository<Admin>, IAdmin
+    {
+        public AdminRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
