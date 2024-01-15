56bd7f64f7d93fd6cac40c491f3fff551b4828f4
Shaima <alqyoodhi181@gmail.com>
2024-01-15 11:10:22 +0400

    updated View Appointment

--- SystemClinc/Controllers/AppointmentController.cs
+++ SystemClinc/Controllers/AppointmentController.cs
@@ -6,12 +6,15 @@ namespace SystemClinc.Controllers
     public class AppointmentController : Controller
     { 
         private readonly IAppointment _Appointment;
-        public AppointmentController(IAppointment appointment) {
+        private readonly ISpecialization _Special;
+        public AppointmentController(IAppointment appointment, ISpecialization special)
+        {
             _Appointment = appointment;
-
+            _Special = special;
         }
         public IActionResult Index()
         {
+            ViewBag.Special = _Special.GetAll();
             return View();
         }
     }
